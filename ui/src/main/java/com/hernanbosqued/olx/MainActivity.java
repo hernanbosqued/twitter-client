092b2ee9c54424610355a22b9182f55afb706cf4
Hernan Bosqued <hernanbosqued@etermax.com>
2017-07-13 22:04:07 -0300

    mvp

--- ui/src/main/java/com/hernanbosqued/olx/MainActivity.java
+++ ui/src/main/java/com/hernanbosqued/olx/MainActivity.java
@@ -1,13 +1,67 @@
 package com.hernanbosqued.olx;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.SearchManager;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends BaseFragmentActivity<MainFragment> implements MainFragment.Callbacks, SearchView.OnQueryTextListener {
+
+    private SearchView searchView;
+    private String query;
+    private final String QUERY_KEY = "query";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected MainFragment getFragment() {
+        return MainFragment.newInstance();
+    }
+
+    @Override
+    protected String getActionBarTitle() {
+        return getString(R.string.app_name);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(QUERY_KEY, searchView.getQuery().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        query = savedInstanceState.getString(QUERY_KEY);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        if (!Utils.isNullOrEmpty(query)) {
+            searchView.setIconified(false);
+            searchView.setQuery(query, false);
+            searchView.clearFocus();
+        }
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        getCurrentFragment().performSearch(query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        //do nothing
+        return false;
     }
 }
