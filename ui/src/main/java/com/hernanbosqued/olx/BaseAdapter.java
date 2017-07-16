5a946905463701e9f14f927084b3312ac69bd9d4
Hernan Bosqued <hernanbosqued@etermax.com>
2017-07-15 23:02:38 -0300

    decorators

--- ui/src/main/java/com/hernanbosqued/olx/BaseAdapter.java
+++ ui/src/main/java/com/hernanbosqued/olx/BaseAdapter.java
@@ -1,21 +1,22 @@
 package com.hernanbosqued.olx;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
 
-abstract class BaseAdapter<M,VH extends BaseViewHolder<M>> extends RecyclerView.Adapter<VH> {
+abstract class BaseAdapter<M, VH extends BaseViewHolder<M>> extends RecyclerView.Adapter<VH> {
 
     private List<M> entities;
 
-    public void setData( List<M> entities){
+    public void setData(@NonNull  List<M> entities) {
         this.entities = entities;
         notifyDataSetChanged();
     }
 
     @Override
-    public void onBindViewHolder(VH holder, int position) {
+    public void onBindViewHolder(@NonNull VH holder, int position) {
         holder.bind(entities.get(position));
     }
 
